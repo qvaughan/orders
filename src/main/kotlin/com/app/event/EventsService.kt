9568876785e01cb2d7f238fbe28bcee9dd6f60ef
Michael Vaughan <jmichaelvaughan@gmail.com>
2021-06-08 20:24:51 -0400

    Add KDocs to EventsService

--- src/main/kotlin/com/app/event/EventsService.kt
+++ src/main/kotlin/com/app/event/EventsService.kt
@@ -1,12 +1,23 @@
 package com.app.event
 
 import com.app.order.Order
-import java.util.concurrent.ConcurrentHashMap
 
+/**
+ * Service that allows components to subscribe to various events emitted by the app.
+ */
 interface EventsService {
 
+    /**
+     * Subscribe to all events associated with any event subscription interfaces [subscriber] may implement.
+     * @param subscriber any object that implements at least one of the associated subscriber interfaces from Subscribers.kt
+     */
     fun subscribe(subscriber: Any)
 
+    /**
+     * Emits an OrderCompleteEvent to subscribers for the given [order].
+     *
+     * @param order the order that has just completed
+     */
     fun orderCompleted(order: Order)
 
 }
